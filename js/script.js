9b686a25c41c6e93e82e7f3efaca71e23974fd1e
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-02-16 00:47:47 +0530

    refactor

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let help = document.querySelectorAll('.help');
-var faq1 = document.querySelector('#faq1');
-let accordion = document.querySelectorAll('.accordion');
-let pnl = document.querySelectorAll('.panel');
+let faq1 = document.querySelector('#faq1');
+let accordions = document.querySelectorAll('.accordion');
+let panels = document.querySelectorAll('.panel');
 let allLines = document.querySelector('#murls-list');
 
 
 function show_help() {
-    var panel1 = faq1.nextElementSibling;
+    let panel1 = faq1.nextElementSibling;
     if (panel1.style.maxHeight) {
         window.open('#faq1', '_parent')
     } else {
@@ -18,46 +18,52 @@ function show_help() {
 }
 
 function open_single() {
-    var url = document.getElementById('surl').value;
-    var num = document.getElementById('url-number').value;
+    let url = document.getElementById('surl').value;
+    let num = document.getElementById('url-number').value;
 
-    if (num > 0 && url != "") {
-        for (var i = 0; i < num; i++) {
-            if (!url) return;
-            if (url.substr(0, 5) != 'http:' && url.substr(0, 6) != 'https:') {
-                url = 'https://' + url;
-            }
-            var urlOpened = window.open(url);
-            if (!urlOpened) {
-                help[0].style.display = 'block';
-            }
+    if (num <= 0) {
+        alert("Enter a valid number (>0)");
+        return;
+    }
 
+    if (!url) {
+        alert('Please enter a url');
+        return;
+    }
+
+    for (let i = 0; i < num; i++) {
+        if (url.substr(0, 5) != 'http:' && url.substr(0, 6) != 'https:') {
+            url = 'https://' + url;
         }
-    } else {
-        alert('Please enter a valid value or enter url');
+        let urlOpened = window.open(url);
+        if (!urlOpened) {
+            help[0].style.display = 'block';
+        }
     }
 }
+
 
 function open_all() {
     let murls = document.getElementById('urls-list').value;
     murls = murls.split(/[\s,]+/);
-    if (murls != "") {
-        for (var i = 0; i < murls.length; i++) {
-            var strLink = murls[i];
-            if (!strLink) return;
-            if (strLink.substr(0, 5) != 'http:' && strLink.substr(0, 6) != 'https:') {
-                strLink = 'https://' + strLink;
-            }
-            allLines.innerHTML += '<div class="murl_text"><a href="' + strLink + '" target="_blank">' + murls[i] + '</a></div>';
-            var WindowOpened = window.open(strLink);
-            if (!WindowOpened) {
-                help[1].style.display = 'block'
-            }
-            WindowOpened = null
 
-        }
-    } else {
+    if (murls == "") {
         alert('Please enter urls');
+        return;
+    }
+
+    for (let i = 0; i < murls.length; i++) {
+        let strLink = murls[i];
+        if (!strLink) return;
+        if (strLink.substr(0, 5) != 'http:' && strLink.substr(0, 6) != 'https:') {
+            strLink = 'https://' + strLink;
+        }
+        allLines.innerHTML += `<div class="murl_text"><a href="${strLink}" target="_blank">${murls[i]}</a></div>`;
+        let WindowOpened = window.open(strLink);
+        if (!WindowOpened) {
+            help[1].style.display = 'block'
+        }
+        WindowOpened = null
     }
 }
 
@@ -66,19 +72,17 @@ document.getElementById('resetall').addEventListener('click', function () {
 });
 
 //accordion
-
-
-for (var i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function () {
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", function () {
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
             this.classList.remove("active")
         } else {
-            Array.from(pnl).forEach(e => e.style.maxHeight = null);
-            Array.from(accordion).forEach(e => e.classList.remove("active"));
+            panels.forEach(e => e.style.maxHeight = null);
+            accordions.forEach(e => e.classList.remove("active"));
             this.classList.add("active");
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
-}
+})
